4cf47ed41409fe0e78d6c7664ed5a6273ca339f8
iqbkam18 <kamranali95@hotmail.com>
2020-08-24 12:02:51 +0200

    add tests

--- src/test/java/dev/exercise/devopsexercise/DevopsExerciseApplicationTests.java
+++ src/test/java/dev/exercise/devopsexercise/DevopsExerciseApplicationTests.java
@@ -3,11 +3,23 @@ package dev.exercise.devopsexercise;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class DevopsExerciseApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void shouldFail (){
+        assertEquals(1, 2);
+    }
+
+    @Test
+    void shouldBeTrue(){
+        assertEquals(1,1);
     }
 
 }
